dd85efef402c72e307e9c9fcb3004d418093a581
Dana Ciocan <danaciocan@economist.com>
2022-08-12 23:14:23 +0100

    Add dark and light mode toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import type { NextPage } from 'next'
-import { Container, Flex, VStack, Heading, Text } from '@chakra-ui/react';
+import { Container, Flex, VStack, Heading, Text, Button, useColorMode } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout/Layout'
 import { getTimeOfDay } from '../utils/get-time-of-day'
 
 const Home: NextPage = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Layout>
       <Container maxW="container.xl">
         <Flex h="100vh" py="20">
-          <VStack w="full" h="full" p={10} spacing={5} alignItems="flex-start" bg="gray.100">
-            <MoonIcon />
-            <SunIcon />
+          <VStack w="full" h="full" p={10} spacing={5} alignItems="flex-start">
+            <Button onClick={toggleColorMode}>
+            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
             <VStack spacing={3} alignItems="flex-start">
               <Text>
                 Good {getTimeOfDay()}!
