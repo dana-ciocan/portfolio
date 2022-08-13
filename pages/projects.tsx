e9d4bfff9c7d2febc7288c3436ce2ac6a02951b7
Dana Ciocan <danaciocan@economist.com>
2022-08-13 07:46:39 +0100

    Create projects page placeholder

---
+++ pages/projects.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import { Container, Flex, VStack, Text } from '@chakra-ui/react';
+import Layout from '../components/Layout/Layout';
+
+const Projects: NextPage = () => {
+  return (
+    <Layout>
+      <Container maxW="container.xl">
+        <Flex h="100vh" py="20">
+          <VStack w="full" h="full" p={10} spacing={5} alignItems="flex-start">
+            <VStack spacing={3} alignItems="flex-start">
+              <Text>This is the projects page</Text>
+            </VStack>
+          </VStack>
+        </Flex>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Projects;
